3ad7cae49d7974bdca2105d3dac25e20c12317a5
odruke-s <odruke-s@student.42lausanne.ch>
2025-06-15 23:01:17 +0200

    improved print function  performance

--- philo/src/print_functions.c
+++ philo/src/print_functions.c
@@ -24,24 +24,48 @@
 // 		printf("%-6ld %i died\n", elapsed_time, philo->id);
 // 	safe_mutex_handle(&data->print_mutex, (t_thrhandle){UNLOCK, __FILE__, __LINE__});
 // }
+
+// void	print_status(t_philo *philo, t_philo_status status)
+// {
+// 	long	elapsed_time;
+// 	t_data	*data;
+
+// 	if (philo->is_full)
+// 		return ;
+// 	data = philo->data;
+// 	elapsed_time = (get_time(MILISECOND) - data->start_sim);
+// 	safe_mutex_handle(&data->print_mutex, (t_thrhandle){LOCK, __FILE__, __LINE__});
+// 	if (status == TAKE_FORK && !get_bool(&data->data_mutex, &data->end_sim))
+// 		printf("%-6ld %i has taken a fork\n", elapsed_time, philo->id);
+// 	else if (status == EATING && !get_bool(&data->data_mutex, &data->end_sim))
+// 		printf("%-6ld %i is eating\n", elapsed_time, philo->id);
+// 	else if (status == SLEEPING && !get_bool(&data->data_mutex, &data->end_sim))
+// 		printf("%-6ld %i is sleeping\n", elapsed_time, philo->id);
+// 	else if (status == THINKING && !get_bool(&data->data_mutex, &data->end_sim))
+// 		printf("%-6ld %i is thinking\n", elapsed_time, philo->id);
+// 	else if (status == DIED && !get_bool(&data->data_mutex, &data->end_sim))
+// 		printf("%-6ld %i died\n", elapsed_time, philo->id);
+// 	safe_mutex_handle(&data->print_mutex, (t_thrhandle){UNLOCK, __FILE__, __LINE__});
+// }
 void	print_status(t_philo *philo, t_philo_status status)
 {
 	long	elapsed_time;
 	t_data	*data;
 
 	data = philo->data;
+	if (philo->is_full || get_bool(&data->data_mutex, &data->end_sim))
+		return ;
 	elapsed_time = (get_time(MILISECOND) - data->start_sim);
-	//check if philo is full?
 	safe_mutex_handle(&data->print_mutex, (t_thrhandle){LOCK, __FILE__, __LINE__});
-	if (status == TAKE_FORK && !get_bool(&data->data_mutex, &data->end_sim))
+	if (status == TAKE_FORK)
 		printf("%-6ld %i has taken a fork\n", elapsed_time, philo->id);
-	else if (status == EATING && !get_bool(&data->data_mutex, &data->end_sim))
+	else if (status == EATING)
 		printf("%-6ld %i is eating\n", elapsed_time, philo->id);
-	else if (status == SLEEPING && !get_bool(&data->data_mutex, &data->end_sim))
+	else if (status == SLEEPING)
 		printf("%-6ld %i is sleeping\n", elapsed_time, philo->id);
-	else if (status == THINKING && !get_bool(&data->data_mutex, &data->end_sim))
+	else if (status == THINKING)
 		printf("%-6ld %i is thinking\n", elapsed_time, philo->id);
-	else if (status == DIED && !get_bool(&data->data_mutex, &data->end_sim))
+	else if (status == DIED)
 		printf("%-6ld %i died\n", elapsed_time, philo->id);
 	safe_mutex_handle(&data->print_mutex, (t_thrhandle){UNLOCK, __FILE__, __LINE__});
 }
